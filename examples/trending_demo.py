b36ef5c5c65850219ca41295d31cb4180118975c
Adam Laiacano <adam@tumblr.com>
2012-08-11 18:17:55 -0400

    didn't need that function.

--- examples/trending_demo.py
+++ examples/trending_demo.py
@@ -1,16 +1,5 @@
-import pycube, datetime, pymongo
-from time import mktime
-import numpy as np
-    
-def generate_trending_data(N = 1000):
-    values = np.random.exponential(100, N)
-    now = datetime.datetime.now()
-    times = [now - datetime.timedelta(0, i) for i in xrange(N)]
-    times = np.array([int(mktime(i.timetuple())) for i in times])
-    
-    return times, values
-
-    
+import pycube
+   
 if __name__ == "__main__":
 
     trend = pycube.trending.TopEvents('actions', '48mp5l', 'act', update_duration=2)
